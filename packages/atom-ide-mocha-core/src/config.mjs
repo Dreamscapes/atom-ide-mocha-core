bc88dc6ca94338d8191124a51cc3707d5abe9676
Robert Rossmann <rr.rossmann@me.com>
2018-11-14 23:51:48 +0100

    chore: clarify docs for interface config

--- packages/atom-ide-mocha-core/src/config.mjs
+++ packages/atom-ide-mocha-core/src/config.mjs
@@ -4,8 +4,8 @@ const config = {
     description: [
       'Preferred communications interface. Unix sockets tend to be generally faster, while IP',
       'allows Atom to receive Mocha reports from anywhere on your local loopback interface (ie.',
-      'from inside Docker).\nNote that for IP, the port will remain the same as long as the',
-      'first folder name in your Atom project remains the same.',
+      'from inside Docker).<br />Both options will always produce the same socket/port per ',
+      'project, even when Atom restarts.',
     ].join(' '),
 
     type: 'string',
