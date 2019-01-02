35ff140d999660e50b455dfa878d4a4f77cfdc05
Robert Rossmann <rr.rossmann@me.com>
2019-01-02 12:12:41 +0100

    chore: refactor the release process 📦

--- utils/mkrelease.sh
+++ utils/mkrelease.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 git pull
-git tag "release@$(date -u +%Y%m%d-%H%M)"
-git push --follow-tags
+npx lerna version
--- utils/release.sh
+++ utils/release.sh
@@ -1,29 +1,9 @@
 #!/usr/bin/env bash
 
-# Set up git user based on last commit's author
-git config user.name "$(git show --quiet --format=%an "${TRAVIS_COMMIT}")"
-git config user.email "$(git show --quiet --format=%ae "${TRAVIS_COMMIT}")"
-
 # Authorise npm for publishing
 cat <<NPMRC >> ~/.npmrc
 //registry.npmjs.org/:_authToken=${NPM_TOKEN}
 NPMRC
 
-# Set up git remote
-git remote add pushable-origin "https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git"
-git checkout "${TRAVIS_BRANCH}"
-
-# Prepare arguments for deployment with lerna
-lernaargs=(
-  --yes
-  --sort
-  --conventional-commits
-  --git-remote pushable-origin
-  --message "chore: release [ci skip]"
-)
-
-# Disable commitlint, for some reason it keeps failing the release process
-rm .git/hooks/commit-msg
-
 # Publish!
-npx lerna publish "${lernaargs[@]}"
+npx lerna publish from-git --yes
